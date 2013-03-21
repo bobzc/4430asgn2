67599ae87ec256493ea2e3fbc79589df730aaea3
Zhou.Chao <bobchou2010@gmail.com>
2013-03-21 13:38:18 +0800

    m2, no mutex

--- myproxy.c
+++ myproxy.c
@@ -3,7 +3,7 @@
 int tn = 0;
 
 HOST_INFO host_info[10];
-
+pthread_mutex_t mutex;
 
 /*******************************************************************************************/
 /*******************************************************************************************/
@@ -91,6 +91,7 @@ void fwd_rpn(int client_fd, int server_fd){
 
 	while(total_count < HEADER_BUFFER_SIZE - 256 &&
 		(count = recv(server_fd, buf + total_count, HEADER_BUFFER_SIZE - 256 - total_count, 0)) > 0){
+		puts("1");
 		total_count += count;
 	}
 	if(total_count <= 0){
@@ -99,7 +100,7 @@ void fwd_rpn(int client_fd, int server_fd){
 	}
 
 	buf_len = proc_rpn(buf, buf + total_count, &cont_len, &header_len);
-	printf("Modified Response header:\n");
+/*	printf("Modified Response header:\n");
 	write(1, buf, header_len);
 	printf("-----------------------------------------\n");
 	printf("Received: %d\n", total_count);
@@ -107,21 +108,20 @@ void fwd_rpn(int client_fd, int server_fd){
 	printf("Response content length: %d\n", cont_len);
 	printf("Response buffer length: %d\n", buf_len);
 	printf("-----------------------------------------\n");
-
+*/
 	/* send response to browser */
 	count = 0;	
 	while(count < buf_len){
 		count += send(client_fd, buf + count, buf_len - count, 0);
 	}
 	int sent_cnt = buf_len - header_len;
-	printf("%d %d\n", sent_cnt, cont_len);
 	while(sent_cnt < cont_len){
 		int recv_cnt = recv(server_fd, buf, sizeof(buf), 0);
 		send(client_fd, buf, recv_cnt, 0);
 		sent_cnt += recv_cnt;
 	}
-	printf("Forwarded content length: %d\n", sent_cnt);
-	printf("-----------------------------------------\n");
+//	printf("Forwarded content length: %d\n", sent_cnt);
+//	printf("-----------------------------------------\n");
 }
 
 
@@ -213,23 +213,23 @@ void milestone_1(int fd){
 	if(total_count <= 0)
 		pthread_exit(NULL);
 
-	printf("Get request header(%d):\n", total_count);
-	write(1, buf, total_count);
-	printf("-----------------------------------------\n");
+//	printf("Get request header(%d):\n", total_count);
+//	write(1, buf, total_count);
+//	printf("-----------------------------------------\n");
 	/* process request header, and get host address */
 	struct addrinfo *host_addr;
 	total_count = proc_req(buf, buf + total_count, &host_addr);
 	
-	printf("Processed request header(%d):\n", total_count);
-	write(1, buf, total_count);
-	printf("-----------------------------------------\n");
+//	printf("Processed request header(%d):\n", total_count);
+//	write(1, buf, total_count);
+//	printf("-----------------------------------------\n");
 
 	int server_fd;
 	if((server_fd = fwd_req(buf, total_count, host_addr)) != -1){
-		printf("Forward request.\n");
+//		printf("Forward request.\n");
 		fwd_rpn(fd, server_fd);
-		printf("Response finished\n");
-		printf("-----------------------------------------\n");
+//		printf("Response finished\n");
+//		printf("-----------------------------------------\n");
 		close(server_fd);
 	}
 	freeaddrinfo(host_addr);
@@ -287,6 +287,30 @@ int is_conn_alive(char *buf_begin, char *buf_end){
 	return 1;
 }
 
+int get_cont_len(char *buf_begin, char *buf_end){
+	char *field_begin = buf_begin - 2;
+	char *field_end = buf_begin - 2;
+	int field_len;
+	do{
+		field_begin = field_end + 2;
+		field_end = strstr(field_begin, "\r\n");
+		field_len = (int)(field_end - field_begin);
+		if(strncmp_i(field_begin, CONTENT_LEN_FIELD, strlen(CONTENT_LEN_FIELD)) == 0){
+			char *val = get_field_value(field_begin + strlen(CONTENT_LEN_FIELD), field_end);
+			return atoi(val);
+		}
+	}while(field_len != 0);
+	return 0;
+}
+
+int get_header_len(char *buf){
+	char *tmp = strstr(buf, "\r\n\r\n");
+	if(tmp == NULL){
+		perror("Bad response.");
+		return 0;
+	}
+	return (int)(tmp - buf) + 4;
+}
 
 /* get host addr */
 void get_host(char *buf_begin, char *buf_end, struct addrinfo **host_addr){
@@ -305,28 +329,40 @@ void get_host(char *buf_begin, char *buf_end, struct addrinfo **host_addr){
 	}while(field_len != 0);
 }
 
+
 int find_fd(struct sockaddr *sa){
+	pthread_mutex_lock(&mutex);
+
 	int fd = 0;
 	int slot = -1;
+
+	/* check if the host is connected */
 	for(int i = 0; i < 10; i++){
 		if(host_info[i].is_taken == 0){
 			slot = i;
 			continue;
 		}else if(sock_addr_cmp_addr(sa, &host_info[i].sa) == 0
 			&& sock_addr_cmp_port(sa, &host_info[i].sa) == 0){
+			printf("Reusing tcp connection...\n");
+			pthread_mutex_unlock(&mutex);
 			return host_info[i].fd;
 		}
 	}
-	
+		
+	/* if not connect to the host */
 	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("Cannot initialize socket.");
+		pthread_mutex_unlock(&mutex);
 		pthread_exit(NULL);
 	}
 	if(connect(fd, (struct sockaddr *)sa, sizeof(struct sockaddr)) == -1){
 		perror("Cannot connect to server.");
+		pthread_mutex_unlock(&mutex);
 		pthread_exit(NULL);
 	}
-		
+	printf("Connecting to host...\n");
+
+	/* allocate a place to store the host info */	
 	if(slot == -1){
 		int slot = rand() % 10;
 	}
@@ -334,49 +370,144 @@ int find_fd(struct sockaddr *sa){
 	host_info[slot].fd = fd;
 	host_info[slot].is_taken = 1;
 	memcpy(&host_info[slot].sa, sa, sizeof(struct sockaddr));
+
+	pthread_mutex_unlock(&mutex);
 	return fd;
+}
+
+void close_conn_host(int fd){
+	pthread_mutex_lock(&mutex);
+	for(int i = 0; i < 10; i++){
+		if(host_info[i].is_taken && host_info[i].fd == fd){
+			host_info[i].is_taken = 0;
+			close(fd);
+			printf("Close connection to host...\n");
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+int recv_req(int fd, char *buf){
+		int total_count = 0, count;
+		do{
+			count = recv(fd, buf + total_count, HEADER_BUFFER_SIZE - total_count, 0);
+			total_count += count;
+		}while(total_count > 0 && strstr(buf, "\r\n\r\n") == NULL);
+
+		if(total_count < 0){
+			perror("Receive request error.");
+			pthread_exit(NULL);
+		}
+		if(total_count == 0){
+			perror("End of request.");
+			pthread_exit(NULL);
+		}
+		printf("Receive requests...(%d Bytes)\n", total_count);
+}
+
+
+int recv_rpn(int server_fd, char *recv_buf){
+		memset(recv_buf, 0, HEADER_BUFFER_SIZE);
+		int total_count = 0;
+//		do{
+		total_count += recv(server_fd, recv_buf + total_count, HEADER_BUFFER_SIZE - 256 - total_count, 0);
+			
+//		}while(total_count != 0 && strstr(recv_buf, "\r\n\r\n") == NULL);
+
+		if(total_count < 0){
+			perror("Read Error.");
+			pthread_exit(NULL);
+		}
+		if(total_count == 0){
+			printf("Connection is down...\n");
+		}	
+
+		return total_count;
 }
 
 
 void milestone_2(int fd){
 	int count, total_count = 0;
-	int conn_flag;
+	int conn_flag = 1;
 	struct addrinfo *host_addr;
 	int server_fd;
 
 	char buf[HEADER_BUFFER_SIZE];
-	memset(buf, 0, HEADER_BUFFER_SIZE);
 	
-	/* receive request */
-	do{
-		count = recv(fd, buf + total_count, HEADER_BUFFER_SIZE - total_count, 0);
-		total_count += count;
-	}while(total_count > 0 && strstr(buf, "\r\n\r\n") == NULL);
+	while(conn_flag){
+		/* receive request */
+		memset(buf, 0, HEADER_BUFFER_SIZE);
+		printf("Waiting for requests...\n");
+		recv_req(fd, buf);
+		
+		char *req_begin = buf;
+		char *req_end = buf;
+		while(1){
+			char *tmp;
+			req_begin = req_end;
+			if((tmp = strstr(req_begin, "\r\n\r\n")) == NULL){
+				break;	
+			}
+			req_end = tmp + 4;
 
-	if(total_count <= 0)
-		pthread_exit(NULL);
+			printf("Handling request...(%d Bytes)\n", (int)(req_end - req_begin));
 
-	conn_flag = is_conn_alive(buf, buf + total_count);
+			/* check connection field */
+			conn_flag = is_conn_alive(req_begin, req_end);
 
-	/* get host address */
-	get_host(buf, buf + total_count, &host_addr);
+			/* get host address */
+			get_host(req_begin, req_end, &host_addr);
+	
+			if(host_addr == NULL){
+				perror("Cannot get host addr.");
+				pthread_exit(NULL);
+			}
+	
+			/* mapping host addr */
+			server_fd = find_fd((struct sockaddr *)host_addr->ai_addr);
 
-	if(host_addr == NULL){
-		perror("Cannot get host addr.");
-		pthread_exit(NULL);
+			/* foward request */
+			send(server_fd, req_begin, (int)(req_end - req_begin), 0);
+			printf("Forward request...(%d Bytes)\n", (int)(req_end - req_begin));
+
+			/* receive response */
+			char recv_buf[HEADER_BUFFER_SIZE];
+			total_count = recv_rpn(server_fd, recv_buf);
+
+			if(total_count == 0){
+				close_conn_host(server_fd);
+				server_fd = find_fd((struct sockaddr *)host_addr->ai_addr);
+				send(server_fd, req_begin, (int)(req_end - req_begin), 0);
+				printf("Re-Forward request...(%d Bytes)\n", (int)(req_end - req_begin));
+				total_count = recv_rpn(server_fd, recv_buf);
+			}	
+
+			/* get content length, header length and check connection field */
+			int cont_len = get_cont_len(recv_buf, recv_buf + total_count);
+			int header_len = get_header_len(recv_buf);
+			int host_conn_flag = is_conn_alive(recv_buf, recv_buf + total_count);
+			
+			count = 0;	
+			int client_fd = fd;
+			while(count < total_count){
+				count += send(client_fd, recv_buf + count, total_count - count, 0);
+			}
+			int sent_cnt = total_count - header_len;
+			while(sent_cnt < cont_len){
+				int recv_cnt = recv(server_fd, recv_buf, sizeof(recv_buf), 0);
+				send(client_fd, recv_buf, recv_cnt, 0);
+				sent_cnt += recv_cnt;
+			}
+			printf("Forward response...");
+			printf("(Header: %d Bytes) (Content: %d Bytes) (Total: %d Bytes)\n", 
+				header_len, cont_len, sent_cnt + header_len);
+	
+			if(host_conn_flag == 0){	
+				close_conn_host(server_fd);
+			}	
+		}
 	}
-	
-	/* mapping host addr */
-	server_fd = find_fd((struct sockaddr *)host_addr->ai_addr);
-
-	
-	send(server_fd, buf, total_count, 0);
-	
-	count = recv(server_fd, buf, HEADER_BUFFER_SIZE, 0);
-	write(fd, buf, count);
 }
-
-
 
 
 
@@ -391,7 +522,7 @@ void milestone_2(int fd){
 
 void cleanup(){
 	printf("thread num: %d\n", --tn);
-	printf("A thread cleans up.\n");
+	printf("\n################# thread end ####################\n");
 }
 
 void *run(void *args){
@@ -404,11 +535,11 @@ void *run(void *args){
 		milestone_1(fd);
 	if(milestone == 2)
 		milestone_2(fd);
-	printf("\n################# thread end ####################\n");
 	pthread_cleanup_pop(1);
 }
 
 int main(int argc, char **argv){
+	pthread_mutex_init(&mutex, NULL);
 	signal(SIGPIPE, SIG_IGN);
 	if(argc != 3){
 		printf("Usage: %s [port] [milestone]\n", argv[0]);
